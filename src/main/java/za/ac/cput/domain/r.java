49451725c336da18eb916ae5b7180270bb9f856b
Tsireledzo Makhado <makhtsire29@gmail.com>
2023-08-05 16:40:12 +0200

    r added

---
+++ src/main/java/za/ac/cput/domain/r.java
@@ -0,0 +1,17 @@
+package za.ac.cput.domain;
+
+public class r {
+    private String name;
+
+    public r(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
